984358658c93a4b6271632b52c423d6bac7a2796
Steve England <steve@wearebeef.co.uk>
2011-10-25 17:44:59 +0100

    Updating noodall templates

--- lib/noodall/templates/config/application.rb
+++ lib/noodall/templates/config/application.rb
@@ -1,15 +1,23 @@
 require File.expand_path('../boot', __FILE__)
 
+<% if include_all_railties? -%>
+require 'rails/all'
+<% else -%>
 # Pick the frameworks you want:
-# require "active_record/railtie"
+<%= comment_if :skip_active_record %>require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-require "rails/test_unit/railtie"
+<%= comment_if :skip_sprockets %>require "sprockets/railtie"
+<%= comment_if :skip_test_unit %>require "rails/test_unit/railtie"
+<% end -%>
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module <%= app_const_base %>
   class Application < Rails::Application
@@ -35,14 +43,19 @@ module <%= app_const_base %>
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(rails)
-
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+<% unless options.skip_sprockets? -%>
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+<% end -%>
 
     config.middleware.insert 0, 'Dragonfly::Middleware', :noodall_assets
   end
